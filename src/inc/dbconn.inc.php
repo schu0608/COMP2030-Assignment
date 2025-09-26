6ea1cdb4a04218d0e84fd868565ae134edd06f81
apsotir12 <soti0015@flinders.edu.au>
2025-09-26 13:52:49 +0930

    Add local fallbacks

--- src/inc/dbconn.inc.php
+++ src/inc/dbconn.inc.php
@@ -1,13 +1,11 @@
 <?php
+$host = getenv('MYSQL_HOST') ?: 'localhost';
+$db   = getenv('MYSQL_DATABASE') ?: 'fussdb';   // your DB name
+$user = getenv('MYSQL_USER') ?: 'root';
+$pass = getenv('MYSQL_PASSWORD') ?: '';         // XAMPP default blank
 
-define("DB_HOST", getenv('MYSQL_HOST'));
-define("DB_NAME", getenv('MYSQL_DATABASE'));
-define("DB_USER", getenv('MYSQL_USER'));
-define("DB_PASS", getenv('MYSQL_PASSWORD'));
-
-$conn = @mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+$conn = @mysqli_connect($host, $user, $pass, $db);
 if (!$conn) {
-    // Something went wrong...
     echo "Error: Unable to connect to database.<br>";
     echo "Debugging errno: " . mysqli_connect_errno() . "<br>";
     echo "Debugging error: " . mysqli_connect_error() . "<br>";
